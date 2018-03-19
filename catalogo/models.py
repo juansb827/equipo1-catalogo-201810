67145789726168d557a0df1dd3b2ce6487d1619d
camendoza94 <ca.mendoza968@uniandes.edu.co>
2018-03-18 19:47:17 -0500

    Added Example model.

--- catalogo/models.py
+++ catalogo/models.py
@@ -20,3 +20,9 @@ class Technology(models.Model):
 
     def __unicode__(self):
         return self.name
+
+
+class Example(models.Model):
+    name = models.CharField(max_length=150)
+    tool = models.ForeignKey(Tool, null=False)
+    technology = models.ForeignKey(Technology, null=False)
