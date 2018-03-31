c2f3ef0a88d25f6390a4336c4cd049dab48d28f3
camendoza94 <ca.mendoza968@uniandes.edu.co>
2018-03-31 13:34:40 -0500

    Test GCG-22

--- catalogo/tests/tests.py
+++ catalogo/tests/tests.py
@@ -15,3 +15,26 @@ class FunctionalTest(TestCase):
     def test_title(self):
         self.browser.get('http://localhost:8000')
         self.assertTrue(u'Catálogo CONECTATE' == self.browser.title)
+
+    def test_add_example(self):
+        self.browser.get('http://localhost:8000')
+        link = self.browser.find_element_by_css_selector("a[href='/catalogo/example/']")
+        link.click()
+
+        name = self.browser.find_element_by_id('name')
+        name.send_keys('Test')
+
+        url = self.browser.find_element_by_id('url')
+        url.send_keys('localhost:8000')
+
+        self.browser.find_element_by_css_selector("#tech > option:nth-child(2)").click()
+
+        self.browser.find_element_by_css_selector("#tool > option:nth-child(2)").click()
+
+        self.browser.find_element_by_css_selector("#formAddExample > button").click()
+
+        self.browser.implicitly_wait(3)
+
+        button = self.browser.find_element_by_css_selector("#formAddExample > button")
+
+        self.assertIn('Guardar', button.text)
